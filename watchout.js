9fb8c56f2dbb70e6c0e0c339a3e33a0b739eda96
mwbennett <bennettwmark@gmail.com>
2015-04-03 19:57:23 -0700

    Wrestling with drag

--- watchout.js
+++ watchout.js
@@ -2,8 +2,6 @@
   // start slingin' some d3 here.
 
   // game options - class variables
-    // window/board dimensions
-    // enemies/players
   var options = {
     width: 700,
     height: 450,
@@ -11,18 +9,23 @@
     padding: 20
   };
 
-    // score/statistics
+  // score/statistics
   var scores = {
     currentScore: 0,
     highScore: 0
   }
+
+  var enemies = [];
+  var player = [];
+
 
   // create board
   var gameBoard = d3.select('body')
     .append('svg:svg')
     .attr('width', options.width)
     .attr('height', options.height)
-    .classed('board', true);
+    .classed('board', true)
+    .classed('backgroundSpace', true);
 
   // var x = d3.scale.linear().domain([0,100]).range([0,options.width]);
   // var y = d3.scale.linear().domain([0,100]).range([0,options.height]);
@@ -34,14 +37,11 @@
     this.id = id;
   };
 
-  var enemies = [];
-
   var generateEnemies = function(){
     for (var i = 0; i < options.enemies; i++){
       var enemy = new Enemy(i);
       enemies.push(enemy);
     }
-    // debugger;
     d3.select('.board').selectAll('svg')
       .data(enemies)
       .enter().append('svg:circle')
@@ -50,8 +50,6 @@
       .attr('r', 10)
       .classed('enemy', true);
   }
-
-  generateEnemies();
 
   // update all enemies
   var updateEnemies = function(){
@@ -71,48 +69,37 @@
     this.y = options.height / 2;
   }
 
-  var player = [];
+
+  var svgPlayer = d3.select('.board').select('.player');
+
+  var drag = d3.behavior.drag()
+    .on('drag', function() {
+      debugger;
+      svgPlayer.attr('cx', d3.event.x).attr('cy', d3.event.y)
+    });
+
+  var dragging = function (d) {
+    d3.select(this)
+      .attr('cx', d.x = d3.event.dx)
+      .attr('cy', d.y = d3.event.dy);
+  }
 
   var generatePlayer = function() {
     player.push(new Player());
-    // debugger;
     d3.select('.board').selectAll('.player')
       .data(player)
       .enter().append('svg:circle')
       .attr('cx', function(d) { return d.x; })
       .attr('cy', function(d) { return d.y; })
       .attr('r', 25)
+      .call(drag)
       .classed('player', true);
   }
 
-  // d3.select('.board').selectAll('.player')
-  //   .data(player)
-  //   .enter().append('svg:circle')
-  //   .attr('cx', function(d) { return d.x; })
-  //   .attr('cy', function(d) { return d.y; })
-  //   .call(drag);
+
+  generateEnemies();
 
   generatePlayer();
-
-  var drag = d3.behavior.drag()
-      .origin(function(d) { return d; })
-      .on("dragstart", dragStarted)
-      .on("drag", dragging)
-      .on("dragend", dragEnded);
-
-  var dragStarted = function (d) {
-    d3.event.sourceEvent.stopPropagation();
-    d3.select(this).classed("dragging", true);
-  }
-
-  var dragging = function (d) {
-    d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
-  }
-
-  var dragEnded = function (d) {
-    d3.select(this).classed("dragging", false);
-  }
-
 
   // functions to do interactions - moving player with mouse
 
